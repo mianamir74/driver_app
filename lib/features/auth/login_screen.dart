68801c8bee864cea0d6ed81888f9a5fdd5cea2c9
Maz <mianamir74@gmail.com>
2026-07-18 17:41:28 +0100

    Build 20: non-async Firebase callbacks - fix libswift_Concurrency crash

--- lib/features/auth/login_screen.dart
+++ lib/features/auth/login_screen.dart
@@ -1,5 +1,5 @@
 import 'dart:async';
-
+// Build 20: non-async Firebase callbacks to fix libswift_Concurrency crash
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
